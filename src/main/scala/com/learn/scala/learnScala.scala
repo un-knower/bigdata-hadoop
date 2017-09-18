efe8d021ca728b568d840c72482b40d7eb1f1a5a
charlesXu86 <charlesxu86@163.com>
2017-09-18 10:20:59 +0800

    学习 scala的 demo

---
+++ src/main/scala/com/learn/scala/learnScala.scala
@@ -0,0 +1,12 @@
+package com.learn.scala
+
+/**
+  * Created by Administrator on 2017/9/18.
+  * Author：charlesXu
+  * file: learnScala.scala
+  * time: 2017/09/18
+  * desc：学习 scala 的 demo
+  */
+object learnScala {
+
+}
