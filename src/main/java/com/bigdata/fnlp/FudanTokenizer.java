74779bbe1ee116625c50c3037b9a70cb24f5aae5
charlesXu86 <charlesxu86@163.com>
2017-09-22 17:34:27 +0800

    fnlp

---
+++ src/main/java/com/bigdata/fnlp/FudanTokenizer.java
@@ -0,0 +1,13 @@
+package com.bigdata.fnlp;
+
+import org.fnlp.nlp.cn.tag.CWSTagger;
+
+/**
+ * Created by Administrator on 2017/9/22.
+ * Author：charlesXu
+ * file: FudanTokenizer
+ * time: 2017/09/22
+ */
+public class FudanTokenizer {
+    private CWSTagger tag;
+}
