79cf137fa2c751e5dcb4d1d8cfb0684f9f3d3c81
charlesXu86 <charlesxu86@163.com>
2017-09-22 11:33:52 +0800

    learn scala

--- src/main/scala/com/learn/scala/learnScala.scala
+++ src/main/scala/com/learn/scala/learnScala.scala
@@ -7,14 +7,26 @@ package com.learn.scala
   * time: 2017/09/18
   * desc：学习 scala 的 demo
   */
+
+
+
 object learnScala {
   def main(args: Array[String]): Unit = {
 
-    var x = 30
-    if (x > 20)
-      print("我爱你")
-    else
-      print("哈哈哈")
+   //判断
+//    var x = 30
+//    if (x > 20)
+//      print("我爱你")
+//    else
+//      print("哈哈哈")
+
+
+
+
+
+
+
+
 
   }
 
