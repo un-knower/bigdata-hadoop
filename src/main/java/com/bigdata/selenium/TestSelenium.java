028d49e4f7370c2c0af1f4a31a75e5c0d9193301
charlesXu86 <charlesxu86@163.com>
2017-10-23 09:30:25 +0800

    自动化测试框架

---
+++ src/main/java/com/bigdata/selenium/TestSelenium.java
@@ -0,0 +1,11 @@
+package com.bigdata.selenium;
+
+/**
+ * Created by Administrator on 2017/10/23.
+ * Author：charlesXu
+ * file: ${Name}.scala
+ * time: 2017/10/23
+ */
+public class TestSelenium {
+
+}
