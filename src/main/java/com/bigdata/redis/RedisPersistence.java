34efd37d8e6aa458688656962ba2d3c841271085
charlesXu86 <charlesxu86@163.com>
2017-10-26 20:31:17 +0800

    redis持久化

---
+++ src/main/java/com/bigdata/redis/RedisPersistence.java
@@ -0,0 +1,10 @@
+package com.bigdata.redis;
+
+/**
+ * Created by Administrator on 2017/10/26.
+ * Author：charlesXu
+ * file: ${Name}.scala
+ * time: 2017/10/26
+ */
+public class RedisPersistence {
+}
